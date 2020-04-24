4aa3d66d6dd7f51f8bdd92d9612337e9263fcb5c
imanthonny <34114220+imanthonny@users.noreply.github.com>
2020-04-23 23:56:16 -0400

    Create download_rnaseq.R

---
+++ download_rnaseq.R
@@ -0,0 +1,6 @@
+#download
+library(downloader)
+basePath <- "https://orcestradata.blob.core.windows.net/ccle/CCLE/2015/RNA-seq"
+download(file.path(basePath, "Kallisto_0.46.1.tar.gz"), destfile="/pfs/out/Kallisto_0.46.1.tar.gz")
+untar("/pfs/out/Kallisto_0.46.1.tar.gz", exdir="/pfs/out/Kallisto_0.46.1")
+file.remove("/pfs/out/Kallisto_0.46.1.tar.gz")
